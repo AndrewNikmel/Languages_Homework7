30301500307c15c1fa55ffc1d15c15accb1ce300
unknown <Andrew M>
2023-01-04 13:05:06 +0300

    Can't find the solution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// Задача 47. Задайте двумерный массив размером m×n, 
 // заполненный случайными вещественными числами.
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
@@ -6,7 +6,7 @@
 // 8 7,8 -7,1 9
 
 
-// // надо еще подумать по отрицательным числам  
+// // // надо еще подумать по отрицательным числам  
 // double [,] Create2dArray(int rows, int cols){    
 //     double [,] array = new double [rows, cols];
 //     Random random = new Random();
@@ -103,65 +103,48 @@
 
 // надо подумать, что не получается
 
-int [,] Create2dArray(int rows, int cols, int min, int max){    
-    int [,] array = new int [rows, cols];
-    Random random = new Random();
-    for(int i = 0; i < rows; i++){                              
-        for(int j = 0; j < cols; j++){                         
-            array[i,j] = random.Next(min, max);         
-        }
-    }
-    return array;
-}
 
-void Show2dArray(int [,] array){                                
-    for(int i = 0; i < array.GetLength(0); i++){
-        for(int j = 0; j < array.GetLength(1); j++){
-            Console.Write(array[i,j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
-
-
-// void SumTheColumns(int [,] array){
-//     int sum = 0;
-//     int i = 0;
-//     int j = 0;
-//     while(i<=array.GetLength(0) && j <= array.GetLength(1)){
-//         sum += array[i,j];
-//         i++;
-//         double find = sum/array.GetLength(1);
-//         double f = Math.Round(find, 1);
-//         Console.Write(f + " ");      
+// int [,] Create2dArray(int rows, int cols, int min, int max){
+//     int [,] array = new int [rows, cols];
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             array [i,j] = new Random().Next(min, max+1);
+//         }
 //     }
-//     j++;
-    
+//     return array;
+// }
 
+// void Show2dArray(int [,] array){
+//     for(int i = 0; i < array.GetLength(0); i++){
+//         for(int j = 0; j < array.GetLength(1); j++){
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-    // double sum = 0;
-    // for(int i = 0; i < array.GetLength(0); i++){
-    //     for(int j = 0; j < array.GetLength(1); j++){
-    //         sum = array[i,j]+array[i++,j];
-             
-    //     }
-    //     double find = sum/array.GetLength(1);
-    //     double f = Math.Round(find, 1);
-    //     Console.Write(f + " ");
-    // }
-}
+// void SumColumns(int [,] array){
+//     int i = 0;
+//     int length = array.GetLength(0);
+//     for(int j = 0; j < array.GetLength(1); j++){
+//         if(i< array.GetLength(0)){
+//             int sum =+ array [i,j]+array[i+1,j];
+//             double arithmeticSum = sum / length;
+//             i++;
+//             Console.Write(arithmeticSum + " ");
+//     }    
+// }
+// }
 
-  
-
-Console.Write("Enter the numb of rows: ");
-int r = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the numb of columns: ");
-int c = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the minimal number of the array: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the maximal number of the array: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int [,] arr = Create2dArray(r,c,min,max);
-Show2dArray(arr);
-Console.WriteLine();
-SumTheColumns(arr);
+// Console.WriteLine("Enter the amount of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the amount of columns: ");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the minimal value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the maximal value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int [,] array = Create2dArray(rows,cols,min,max);
+// Show2dArray(array);
+// Console.WriteLine();
+// SumColumns(array);
